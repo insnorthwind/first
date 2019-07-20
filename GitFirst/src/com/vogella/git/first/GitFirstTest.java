36d9b4541b1d1574d6187a583f8b113dc41eb401
insnorthwind <insnorthwind@hotmail.com>
2019-07-20 11:10:29 +0800

    Change after rollback

--- GitFirst/src/com/vogella/git/first/GitFirstTest.java
+++ GitFirst/src/com/vogella/git/first/GitFirstTest.java
@@ -4,5 +4,6 @@ public class GitFirstTest {
 	public static void main(String[] args) {
 		System.out.println("First Git");
 		System.out.println("Git is really cool!");
+		System.out.println("This is change after rollback");
 	}
 }
