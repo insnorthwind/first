2cf89602f054d8addf2df93fa7082312a0d07c8c
insnorthwind <insnorthwind@hotmail.com>
2019-07-20 09:46:01 +0800

    Adjust example output message

--- GitFirst/src/com/vogella/git/first/GitFirstTest.java
+++ GitFirst/src/com/vogella/git/first/GitFirstTest.java
@@ -3,5 +3,6 @@ package com.vogella.git.first;
 public class GitFirstTest {
 	public static void main(String[] args) {
 		System.out.println("First Git");
+		System.out.println("Git is really cool!");
 	}
 }
